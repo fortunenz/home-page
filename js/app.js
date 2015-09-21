0ff84a412d2b54b94101a823716329e37a8f9173
Brian Chen <bchen.chen93@gmail.com>
2015-09-21 14:17:49 +1200

    feat: Changed the page to a signup page, only I have access to create new users

--- js/app.js
+++ js/app.js
@@ -1,27 +1,60 @@
 (function() {
+  Parse.initialize("p45yej86tibQrsfKYCcj6UmNw4o7b6kxtsobZnmA", "fXSkEhDGakCYnVv5OOdAfWDmjAuQvlnFI5KOwIUO");
+
   var app = angular.module("homePageApp", []);
 
-  app.controller("mainController", function() {
+  app.controller("mainController", function($scope) {
     var self = this;
-    self.access = false;
+    self.logged = false;
+    self.loggedName = "";
+    self.loggedPass = "";
+    self.user;
+
+    self.firstName = "";
+    self.lastName = "";
     self.userName = "";
     self.password = "";
-    self.users = model.users;
+    self.email = "";
+    self.access = "";
 
     self.login = function() {
-
-      for (i = 0, len = self.users.length; i < len; i++) {
-        if (self.users[i].userName.toLowerCase() == self.userName.toLowerCase() && self.users[i].password == self.password) {
-          self.access = true;
+      Parse.User.logIn(self.loggedName, self.loggedPass, {
+        success: function(user) {
+          self.logged = true;
+          self.user = user;
+          $scope.$apply();
+          // Do stuff after successful login.
+        },
+        error: function(user, error) {
+          // The login failed. Check error to see why.
         }
-      }
-    }
-  });
+      });
+    };
 
-  app.controller("functionController", function() {
-    var self = this;
-    self.sites = model.sites;
-
-
+    self.signUp = function() {
+      var newUser = new Parse.User();
+      newUser.set("username", self.userName);
+      newUser.set("password", self.password);
+      newUser.set("firstName", self.firstName);
+      newUser.set("lastName", self.lastName);
+      newUser.set("email", self.email);
+      newUser.set("access", self.access);
+      newUser.signUp(null, {
+        success: function(newUser) {
+          // Hooray! Let them use the app now.
+          alert("It worked!");
+          self.firstName = "";
+          self.lastName = "";
+          self.userName = "";
+          self.password = "";
+          self.email = "";
+          self.access = "";
+        },
+        error: function(newUser, error) {
+          // Show the error message somewhere and let the user try again.
+          alert("Error: " + error.code + " " + error.message);
+        }
+      });
+    };
   });
 })();
