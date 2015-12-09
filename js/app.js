43a5b5a7a7efad4f91faabd04ceb69457115aa6f
Brian Chen <bchen.chen93@gmail.com>
2015-12-09 13:34:13 +1300

    feat: Application now checks if customer short names or item codes are already taken locally

--- js/app.js
+++ js/app.js
@@ -81,29 +81,43 @@
 
     self.create = function() {
       var newItem = new Items();
-      newItem.set("code", self.code);
-      newItem.set("description", self.description);
-      newItem.set("unit", self.unit);
-      newItem.set("quantity", parseInt(self.quantitiy));
-      newItem.set("price", parseInt(self.defaultPrice));
-      newItem.set("orderAs", self.orderAs);
-      newItem.save(null, {
-        success: function(item) {
-          console.log('New object created with objectId: ' + item.id);
-          alert("Your new item has been created!")
-          self.code = "";
-          self.description = "";
-          self.unit = "";
-          self.quantitiy = "";
-          self.packaging = "";
-          self.defaultPrice = 0;
-          self.orderAs = "";
-          $scope.$apply();
+      var itemQuery = new Parse.Query(Items);
+
+      itemQuery.equalTo("code", self.code.toUpperCase());
+      itemQuery.first({
+        success: function(results) {
+          if (results !== undefined) {
+            alert("Sorry, this code is already taken. Please use a different code");
+            return;
+          }
+          newItem.set("code", self.code.toUpperCase());
+          newItem.set("description", self.description);
+          newItem.set("unit", self.unit);
+          newItem.set("quantity", parseInt(self.quantitiy));
+          newItem.set("price", parseInt(self.defaultPrice));
+          newItem.set("orderAs", self.orderAs.toLowerCase());
+          newItem.save(null, {
+            success: function(item) {
+              console.log('New object created with objectId: ' + item.id);
+              alert("Your new item has been created!")
+              self.code = "";
+              self.description = "";
+              self.unit = "";
+              self.quantitiy = "";
+              self.packaging = "";
+              self.defaultPrice = 0;
+              self.orderAs = "";
+              $scope.$apply();
+            },
+            error: function(item, error) {
+              // Execute any logic that should take place if the save fails.
+              // error is a Parse.Error with an error code and message.
+              console.log('Failed to create new object, with error code: ' + error.message);
+            }
+          });
         },
-        error: function(item, error) {
-          // Execute any logic that should take place if the save fails.
-          // error is a Parse.Error with an error code and message.
-          console.log('Failed to create new object, with error code: ' + error.message);
+        error: function(error) {
+          console.log("Error: " + error.code + " " + error.message);
         }
       });
     }
@@ -122,35 +136,48 @@
     self.shippingComment = "";
 
     self.create = function() {
-      console.log("hihi from customers");
       var newCustomer = new Customers();
-      newCustomer.set("name", self.name);
-      newCustomer.set("short", self.short);
-      newCustomer.set("acc", self.acc);
-      newCustomer.set("address", self.address);
-      newCustomer.set("city", self.city);
-      newCustomer.set("type", self.type);
-      if (self.shippingComment.trim().length !== 0) {
-        newCustomer.set("shippingComment", self.shippingComment);
-      }
-      newCustomer.save(null, {
-        success: function(customer) {
-          console.log('New object created with objectId: ' + customer.id);
-          alert("Your new customer has been created!")
-          self.name = "";
-          self.short = "";
-          self.acc = "";
-          self.address = "";
-          self.city = "";
-          self.type = "";
-          self.includeGST = false;
-          self.shippingComment = "";
-          $scope.$apply();
+      var customerQuery = new Parse.Query(Customers);
+
+      customerQuery.equalTo("short", self.short);
+      customerQuery.first({
+        success: function(results) {
+          if (results !== undefined) {
+            alert("Sorry, this short name is already taken. Please use a different name");
+            return;
+          }
+          newCustomer.set("name", self.name);
+          newCustomer.set("short", self.short);
+          newCustomer.set("acc", self.acc);
+          newCustomer.set("address", self.address);
+          newCustomer.set("city", self.city);
+          newCustomer.set("type", self.type);
+          if (self.shippingComment.trim().length !== 0) {
+            newCustomer.set("shippingComment", self.shippingComment);
+          }
+          newCustomer.save(null, {
+            success: function(customer) {
+              console.log('New object created with objectId: ' + customer.id);
+              alert("Your new customer has been created!")
+              self.name = "";
+              self.short = "";
+              self.acc = "";
+              self.address = "";
+              self.city = "";
+              self.type = "";
+              self.includeGST = false;
+              self.shippingComment = "";
+              $scope.$apply();
+            },
+            error: function(customer, error) {
+              // Execute any logic that should take place if the save fails.
+              // error is a Parse.Error with an error code and message.
+              console.log('Failed to create new object, with error code: ' + error.message);
+            }
+          });
         },
-        error: function(customer, error) {
-          // Execute any logic that should take place if the save fails.
-          // error is a Parse.Error with an error code and message.
-          console.log('Failed to create new object, with error code: ' + error.message);
+        error: function(error) {
+          console.log("Error: " + error.code + " " + error.message);
         }
       });
     }
