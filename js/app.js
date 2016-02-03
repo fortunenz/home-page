b77ab4f88e875a889d0fcda47a0b1ab300e2e7e6
Brian Chen <bchen.chen93@gmail.com>
2016-02-03 17:05:43 +1300

    feat: All craetions are done using firebase server

--- js/app.js
+++ js/app.js
@@ -1,74 +1,65 @@
 (function() {
-  Parse.initialize("p45yej86tibQrsfKYCcj6UmNw4o7b6kxtsobZnmA", "fXSkEhDGakCYnVv5OOdAfWDmjAuQvlnFI5KOwIUO");
-  var Items = Parse.Object.extend("Items");
-  var Customers = Parse.Object.extend("Customers");
-
-  var app = angular.module("homePageApp", []);
+  var app = angular.module("homePageApp", ["firebase"]);
+  // Connects to the firebase server
+  var ref = new Firebase('https://popping-torch-7294.firebaseio.com/');
 
   app.controller("mainController", function($scope) {
+    // Firebase queries ----------------------------------------------------------
+    ref.onAuth(function(authData) {
+      $scope.access = false;
+      if (authData) {
+        console.log("User " + authData.uid + " is logged in with " + authData.provider);
+        $scope.access = true;
+      } else {
+        console.log("Client unauthenticated.")
+      }
+    });
+
     var self = this;
 
-    var currentUser = Parse.User.current();
-    if (currentUser) {
-      self.logged = true;
-    } else {
-      self.logged = false;
-    }
-
-    self.loggedName = "";
+    self.loggedEmail = "";
     self.loggedPass = "";
     self.user;
 
-    self.firstName = "";
-    self.lastName = "";
-    self.userName = "";
-    self.password = "";
     self.email = "";
-    self.access = "";
+    self.password = "";
 
     self.login = function() {
-      Parse.User.logIn(self.loggedName, self.loggedPass, {
-        success: function(user) {
-          self.logged = true;
-          self.user = user;
-          $scope.$apply();
-          // Do stuff after successful login.
-        },
-        error: function(user, error) {
-          // The login failed. Check error to see why.
+      ref.authWithPassword({
+        email    : self.loggedEmail,
+        password : self.loggedPass
+      }, function(error, authData) {
+        if (error) {
+          console.log("Login Failed!", error);
+        } else {
+          console.log("Authenticated successfully with payload:", authData);
+          $scope.access = true;
         }
       });
     };
 
     self.signUp = function() {
-      var newUser = new Parse.User();
-      newUser.set("username", self.userName);
-      newUser.set("password", self.password);
-      newUser.set("firstName", self.firstName);
-      newUser.set("lastName", self.lastName);
-      newUser.set("email", self.email);
-      newUser.set("access", self.access);
-      newUser.signUp(null, {
-        success: function(newUser) {
-          console.log(newUser);
+      ref.createUser({
+        email    : self.email,
+        password : self.password
+      }, function(error, userData) {
+        if (error) {
+          console.log("Error creating user:", error);
+        } else {
+          console.log("Successfully created user account with uid:", userData.uid);
           alert("New user created!");
-          self.firstName = "";
-          self.lastName = "";
-          self.userName = "";
-          self.password = "";
           self.email = "";
-          self.access = "";
+          self.password = "";
           $scope.$apply();
-        },
-        error: function(newUser, error) {
-          // Show the error message somewhere and let the user try again.
-          console.log("Error: " + error.code + " " + error.message);
         }
       });
     };
   });
 
-  app.controller("itemController", function($scope) {
+  app.controller("itemController", function($scope, $firebaseArray) {
+    var itemsRef = new Firebase('https://popping-torch-7294.firebaseio.com/items');
+    $scope.items = $firebaseArray(itemsRef);
+
     var self = this;
 
     self.code = "";
@@ -80,51 +71,47 @@
     self.orderAs = "";
 
     self.create = function() {
-      var newItem = new Items();
-      var itemQuery = new Parse.Query(Items);
-
-      itemQuery.equalTo("code", self.code.toUpperCase());
-      itemQuery.first({
-        success: function(results) {
-          if (results !== undefined) {
-            alert("Sorry, this code is already taken. Please use a different code");
-            return;
-          }
-          newItem.set("code", self.code.toUpperCase());
-          newItem.set("description", self.description);
-          newItem.set("unit", self.unit);
-          newItem.set("quantity", parseInt(self.quantitiy));
-          newItem.set("packaging", parseInt(self.packaging));
-          newItem.set("price", parseInt(self.defaultPrice));
-          newItem.set("orderAs", self.orderAs.toLowerCase());
-          newItem.save(null, {
-            success: function(item) {
-              console.log('New object created with objectId: ' + item.id);
-              alert("Your new item has been created!")
-              self.code = "";
-              self.description = "";
-              self.unit = "";
-              self.quantitiy = "";
-              self.packaging = "";
-              self.defaultPrice = 0;
-              self.orderAs = "";
-              $scope.$apply();
-            },
-            error: function(item, error) {
-              // Execute any logic that should take place if the save fails.
-              // error is a Parse.Error with an error code and message.
-              console.log('Failed to create new object, with error code: ' + error.message);
-            }
-          });
-        },
-        error: function(error) {
-          console.log("Error: " + error.code + " " + error.message);
+      var codeError = false;
+      for (var i = 0, len = $scope.items.length; i < len; i++) {
+        if ($scope.items[i].code == self.code.toUpperCase()) {
+          codeError = true;
         }
-      });
+      }
+
+      if (codeError === false) {
+        ref.child("items").push({
+          "code": self.code.toUpperCase(),
+          "description": self.description,
+          "unit": self.unit,
+          "quantity": parseInt(self.quantitiy),
+          "packaging": self.packaging,
+          "price": parseInt(self.defaultPrice),
+          "orderAs": self.orderAs.toLowerCase()
+        }, function(error) {
+          if (error) {
+            console.log("Data could not be saved." + error);
+          } else {
+            alert("Your new item has been created!")
+            self.code = "";
+            self.description = "";
+            self.unit = "";
+            self.quantitiy = "";
+            self.packaging = "";
+            self.defaultPrice = 0;
+            self.orderAs = "";
+            $scope.$apply();
+          }
+        });
+      } else {
+        alert("You alraedy have that code in your database please try another");
+      }
     }
   });
 
-  app.controller("customerController", function($scope) {
+  app.controller("customerController", function($scope, $firebaseArray) {
+    var customersRef = new Firebase('https://popping-torch-7294.firebaseio.com/customers');
+    $scope.customers = $firebaseArray(customersRef);
+
     var self = this;
 
     self.name = "";
@@ -137,50 +124,45 @@
     self.shippingComment = "";
 
     self.create = function() {
-      var newCustomer = new Customers();
-      var customerQuery = new Parse.Query(Customers);
-
-      customerQuery.equalTo("short", self.short);
-      customerQuery.first({
-        success: function(results) {
-          if (results !== undefined) {
-            alert("Sorry, this short name is already taken. Please use a different name");
-            return;
-          }
-          newCustomer.set("name", self.name);
-          newCustomer.set("short", self.short);
-          newCustomer.set("acc", self.acc);
-          newCustomer.set("address", self.address);
-          newCustomer.set("city", self.city);
-          newCustomer.set("type", self.type);
-          if (self.shippingComment.trim().length !== 0) {
-            newCustomer.set("shippingComment", self.shippingComment);
-          }
-          newCustomer.save(null, {
-            success: function(customer) {
-              console.log('New object created with objectId: ' + customer.id);
-              alert("Your new customer has been created!")
-              self.name = "";
-              self.short = "";
-              self.acc = "";
-              self.address = "";
-              self.city = "";
-              self.type = "";
-              self.includeGST = false;
-              self.shippingComment = "";
-              $scope.$apply();
-            },
-            error: function(customer, error) {
-              // Execute any logic that should take place if the save fails.
-              // error is a Parse.Error with an error code and message.
-              console.log('Failed to create new object, with error code: ' + error.message);
-            }
-          });
-        },
-        error: function(error) {
-          console.log("Error: " + error.code + " " + error.message);
+      var codeError = false;
+      for (var i = 0, len = $scope.customers.length; i < len; i++) {
+        if ($scope.customers[i].short == self.short) {
+          codeError = true;
         }
-      });
+      }
+
+      if (self.shippingComment.trim().length === 0) {
+        self.shippingComment = null;
+      }
+
+      if (codeError === false) {
+        ref.child("customers").push({
+          "name": self.name,
+          "short": self.short,
+          "acc": self.acc,
+          "address": self.address,
+          "city": self.city,
+          "type": self.type,
+          "shippingComment": self.shippingComment
+        }, function(error) {
+          if (error) {
+            console.log("Data could not be saved." + error);
+          } else {
+            alert("Your new customer has been created!")
+            self.name = "";
+            self.short = "";
+            self.acc = "";
+            self.address = "";
+            self.city = "";
+            self.type = "";
+            self.includeGST = false;
+            self.shippingComment = "";
+            $scope.$apply();
+          }
+        });
+      } else {
+        alert("Sorry, this short name is already taken. Please use a different name");
+      }
     }
   });
 })();
